7240cde6b4027715c84b0250a2b0c48892c9152b
Vitor Marcondes <vitorsergio111@gmail.com>
2022-09-22 14:02:18 -0300

    done comments + optmzations

--- fractol.c
+++ fractol.c
@@ -1,64 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 11:02:04 by vsergio           #+#    #+#             */
+/*   Updated: 2022/09/22 14:01:45 by vsergio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fractol.h"
-#include <unistd.h>
+
 int mandelbrot(double ci, double cr, int times);
 int main(int argc, char **argv)
 {
-	int		line;
-	int		column;
-	double	cr;
-	double	ci;
-	double	min_r;
-	double	max_r;
-	double	min_i;
-	double	max_i;
-	int		largura;
-	int		altura;
-	min_r = -2.0;
-	max_r = 1.0;
-	min_i = -1.5;
-	max_i = 1.5;
-	largura = 100; // -> pixels largura;
-	altura = 100; // -> pixels altura;
+	t_positions points;
+	t_complex c;
+	int		height;
+	int		width;
+	int		i_pos;
+	int		r_pos;
 
-	line = 0;
-	while(line < largura)
+	argv = 0;
+	if (argc != 1)
 	{
-		column = 0;
-		ci = min_r + line * (max_r - min_r) / largura; // aqui eu decubro a posicao real de C de acordo com o tamanho de pixels;
-		// o imaginario quebra a linha pois ele vai de cima para baixo, pq o eixo eh em pe
-		while(column < altura)
+		write(2, "Error\n", 6);
+		exit(EXIT_FAILURE);
+	}
+	points.min_r = -2.0;
+	points.max_r = 1.0;
+	points.min_i = -1.5;
+	points.max_i = 1.5;
+	width = 110; // -> pixels largura;
+	height = 110; // -> pixels altura;
+	i_pos = 0;
+	while(i_pos < height)
+	{
+		r_pos = 0;
+		c.i = points.min_r + i_pos * (points.max_r - points.min_r) / height; // posicao real de C de acordo com o tamanho de pixels + o tamanho no plano (-2.0 ate 1.0, diminuindo 0.3 (distancia entre os pontos / largura) a cada passada ());
+		while(r_pos < width)
 		{
-			cr = min_i + column * (max_i - min_i) / altura; // aqui eu descubro a posicao imaginaria do C de acordo com tamanho de pixels
-			//aqui o real ira andar para a direita, ate o final da linha, pois o eixo eh horizontal e vai da esquerda pra direita
-			if (mandelbrot(cr, ci, 200) == 0)
+			c.r = points.min_i + r_pos * (points.max_i - points.min_i) / width; // posicao imaginaria do C de acordo com tamanho de pixels e a posicao do plano (-1.5 a 1.5) diminuindo 0.3 (distancia entre os pontos / altura) a cada passada;
+			//o real ira andar para a direita, ate o final da linha, pois o eixo eh horizontal e vai da esquerda pra direita
+			if (mandelbrot(c.r, c.i, 200) == 0)
 				write(1, " ", 1);
 			else
 				write(1, ".", 1);
-			column++;
+			r_pos++;
 		}
 		write(1, "\n", 1);
-		line++;
+		// o imaginario quebra a linha pois o eixo eh vertical e vai descendo
+		i_pos++;
 	}
 }
 
-int mandelbrot(double cr, double ci, int times)
+int mandelbrot(double cr, double ci, int iterations)
 {
-	double zr;
-	double zi;
+	t_complex z;
 	double temp;
-	int iterator; 
+	int counter; 
+	double zrsqr;
+	double zisqr;
 
-	iterator = 0;
-	zr = 0;
-	zi = 0;
-	while(iterator < times)
+	counter = 0;
+	z.r = 0;
+	z.i = 0;
+	zrsqr = z.r * z.r;
+	zisqr = z.i * z.i;
+	while(counter < iterations)
 	{
-		if (sqrt(zr * zr + zi * zi) > 2) // checa se ira tender ao infinito
+		if (sqrt(z.r * z.r + z.i * z.i) > 2) // checa se ira tender ao infinito
 			return (0);
-		temp = 2 * zr * zi + ci; // imaginario ao quadrado + ci -> z(i)^2 + c(i)
-		zr = zr * zr - zi * zi + cr; //real ao quadrado + cr -> z(r)^2 + c(r)
-		zi = temp;
-		iterator++;
+		temp = z.r * z.i * 2 + ci; // imaginario ao quadrado + ci -> z(i)^2 + c(i)
+		z.r = z.r * z.r - z.i * z.i + cr; //real ao quadrado + cr -> z(r)^2 + c(r)
+		//calcuo pra descobrir o quadrado de numeros complexos
+		z.i = temp;
+		counter++;
 	}
 	return (1);
 }
--- fractol.h
+++ fractol.h
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 11:02:06 by vsergio           #+#    #+#             */
+/*   Updated: 2022/09/22 11:02:07 by vsergio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
 # include <math.h>
-# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_complex
 {
@@ -10,4 +23,11 @@ typedef struct s_complex
 	double i;
 }				t_complex;
 
+typedef struct s_positions
+{
+	double min_r;
+	double max_r;
+	double min_i;
+	double max_i;
+}				t_positions;
 #endif
