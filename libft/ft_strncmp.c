c0842911de7003653bad604f92885115bb620717
Vitor Marcondes <vitorsergio111@gmail.com>
2022-10-03 17:22:10 -0300

    handled burningship error

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 22:37:36 by vsergio           #+#    #+#             */
-/*   Updated: 2022/10/03 17:12:09 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/10/03 17:21:29 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	if (n == 5 && ft_strlen(str1) != 5)
 		return (1);
 	else if (n == 10 && ft_strlen(str1) != 10)
+		return (1);
+	else if (n == 11 && ft_strlen(str1) != 11)
 		return (1);
 	while (str1[i] == str2[i] && str1[i] != '\0'
 		&& str2[i] != '\0' && i < n - 1)
